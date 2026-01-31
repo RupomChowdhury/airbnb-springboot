4a4baff0ce924c6a3cb51e5e1f1153aef8f10690
RupomChowdhury <web.rupom@gmail.com>
2026-01-31 19:10:21 +0600

    Initial project setup with Hotel and Room Entities.

---
+++ src/main/java/dev/rupom/project/airbnb/entity/Hotel.java
@@ -0,0 +1,46 @@
+package dev.rupom.project.airbnb.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    private Boolean active;
+
+    @Embedded
+    private ContactInfo contactInfo;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Room> room = new ArrayList<>();
+}
